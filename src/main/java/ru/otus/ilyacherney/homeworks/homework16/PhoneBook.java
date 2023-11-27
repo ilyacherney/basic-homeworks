d8174e08e3ccd714d7ce39a8f7c9b92ea866ad77
Ilya Cherney <ilyacherney@yandex.ru>
2023-11-27 15:40:56 +0300

    внёс правки

--- src/main/java/ru/otus/ilyacherney/homeworks/homework16/PhoneBook.java
+++ src/main/java/ru/otus/ilyacherney/homeworks/homework16/PhoneBook.java
@@ -3,34 +3,34 @@ package ru.otus.ilyacherney.homeworks.homework16;
 import java.util.*;
 
 public class PhoneBook {
-    public Map<String, ArrayList<Long>> map;
+    public Map<String, Set<String>> map;
     public PhoneBook() {
         this.map = new HashMap<>();
     }
 
-    public void add(String name, long number) {
-        if (find(name) == null) {
-            List<Long> numbers = new ArrayList<>();
-            numbers.add(number);
-            map.put(name, (ArrayList<Long>) numbers);
+    public void add(String name, String number) {
+        if (map.containsKey(name)) {
+            Set<String> existingNumbers = map.get(name);
+            existingNumbers.add(number);
         } else {
-            List<Long> numbers = map.get(name);
-            numbers.add(number);
+            Set<String> newNumbers = new HashSet<>();
+            newNumbers.add(number);
+            map.put(name, newNumbers);
         }
     }
 
-    public String find(String name) {
+    public Set<String> find(String name) {
         try {
-            return map.get(name).toString();
+            return map.get(name);
         } catch (NullPointerException e) {
             return null;
         }
     }
 
-    public boolean containsPhoneNumber(long number) {
-        for (Map.Entry<String, ArrayList<Long>> entry : map.entrySet())
-            if (entry.getValue().contains(number))
-                return true;
+    public boolean containsPhoneNumber(String number) {
+        for (Set<String> value: map.values()) {
+            if (value.contains(number)) return true;
+        }
         return false;
     }
 }
